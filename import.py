f96412344ccd9b7e4f5dfafc54b3e089efb02efd
Luke Swanson <lwswanson2@wisc.edu>
2022-12-28 10:03:07 -0600

    Ok, one more renaming of files.
    Now, we'll have main.py for main code, import for classes related to
    importing the file, and export for classes related to setting up the
    blend file.

--- import.py
+++ import.py
@@ -1,20 +1,151 @@
 # builtin imports
-from pathlib import Path
-from typing import Any, Dict
-from xml.etree import ElementTree as ET
+from typing import Any, Dict, Optional
 from xml.etree.ElementTree import ElementTree, Element
-# 3rd-party imports
-import bpy
-# local imports
-from utils import iMovieProj, XMLDict
 
-X_RESOLUTION = 640
-Y_RESOLUTION = 480
-BASE_FILEPATH = Path("/Volumes/Little_Black_Box/LBB Users/LBB lukeswanson/LBB Movies")
+class XMLDict:
+    @staticmethod
+    def XMLTreeToDict(xmltree:ElementTree):
+        root = xmltree.getroot()
+        if len(root) > 1:
+            # file should have only one dict
+            raise ValueError(f"Expected only one element (a dict) under iMovieProj XML root, found {len(root)}!")
+        xmldict = root[0]
+        if xmldict.tag != "dict":
+            raise ValueError(f"Expected a dict under iMovieProj XML root, found {xmldict.tag}!")
+        return XMLDict._parseDict(xmldict)
 
-path_of_proj_file = BASE_FILEPATH / Path("Outdoor_Movie/outdoor_movie_edit.iMovieProj")
-path_of_clips = BASE_FILEPATH / Path("Outdoor_Movie/")
+    @staticmethod
+    def _parse(xmlvalue:Element):
+        if xmlvalue.tag == "true":
+            return True
+        elif xmlvalue.tag == "false":
+            return False
+        else:
+            if xmlvalue.text is None:
+                raise ValueError(f"Expected XML Element with tag {xmlvalue.tag} to have a value, but found a value of None!")
+            elif xmlvalue.tag == "string":
+                return str(xmlvalue.text)
+            elif xmlvalue.tag == "integer":
+                return int(xmlvalue.text)
+            elif xmlvalue.tag == "real":
+                return float(xmlvalue.text)
+            elif xmlvalue.tag == "array":
+                return [XMLDict._parse(elem) for elem in xmlvalue]
+            elif xmlvalue.tag == "dict":
+                return XMLDict._parseDict(xmlvalue)
 
-xmltree : ElementTree    = ET.parse(path_of_proj_file)
-xmldict : Dict[str, Any] = XMLDict.XMLTreeToDict(xmltree)
-project : iMovieProj     = iMovieProj(xmldict=xmldict)
+    @staticmethod
+    def _parseDict(xmldict:Element):
+        ret_val = {}
+
+        if len(xmldict) % 2 != 0:
+            raise ValueError(f"Expected the XML dictionary to have an even number of elements, found {len(xmldict)} elements!")
+        for i in range(len(xmldict) // 2):
+            if xmldict[2*i].tag != "key":
+                raise ValueError(f"Expected the even keys under XML dict to be keys, found element {2*i} with tag {xmldict[2*i].tag}!")
+            key = xmldict[2*i].text
+            val = XMLDict._parse(xmldict[2*i + 1])
+            ret_val[key] = val
+
+        return ret_val
+
+class VideoClip:
+    """Tracking of video clip properties"""
+    def __init__(self):
+        self._duration = 0
+        self._fileName = "empty"
+        self._sceneName = "empty"
+        self._inFrame = 0
+        self._outFrame = 0
+        self._partOfEdit = 0
+
+    @property
+    def Duration(self):
+        return self._duration
+    @property
+    def FileName(self):
+        return self._fileName
+    @property
+    def SceneName(self):
+        return self._sceneName
+    @property
+    def InFrame(self):
+        return self._inFrame
+    @property
+    def OutFrame(self):
+        return self._outFrame
+    @property
+    def InEdit(self):
+        """Method to check if the current clip is part of the edit."""
+        if self._partOfEdit == 0:
+            return False
+        elif self._partOfEdit == 1:
+            return True
+        else:
+            return False
+
+    def toString(self):
+        return "scene " + self._sceneName + ", file " + self._fileName + ", in edit: " + str(self._partOfEdit)
+
+class iMovieProj:
+    """Class to handle structure of an iMovieHD project
+    """
+
+    def __init__(self, xmldict:Dict[str, Any]):
+        nested_keys = ["audioClips", "audioTrashClips", "videoClips", "videoTrashClips"]
+        self._other_elements = {key:xmldict[key] for key in xmldict.keys() if key not in nested_keys}
+        self._audioClips      = self._getAudioClips(xmltree)
+        self._audioTrashClips = self._getAudioTrashClips(xmltree)
+        self._videoClips      = self._getVideoClips(xmltree)
+        self._videoTrashClips = self._getVideoTrashClips(xmltree)
+
+    @property
+    def AudioClips(self):
+        return self._audioClips
+    @property
+    def AudioTrashClips(self):
+        return self._audioTrashClips
+    @property
+    def LastClipUniqueID(self) -> Optional[int]:
+        return self._other_elements.get("lastClipUniqueID")
+    @property
+    def PlayheadPosition(self) -> Optional[int]:
+        return self._other_elements.get("playheadPosition")
+    @property
+    def RelativePlayHeadPosition(self) -> Optional[float]:
+        return self._other_elements.get("relativePlayHeadPosition")
+    @property
+    def SelectionEndFrame(self) -> Optional[int]:
+        return self._other_elements.get("selectionEndFrame")
+    @property
+    def SelectionStartFrame(self) -> Optional[int]:
+        return self._other_elements.get("selectionStartFrame")
+    @property
+    def SelectionType(self) -> Optional[int]:
+        return self._other_elements.get("selectionType")
+    @property
+    def TimelineZoom(self) -> Optional[float]:
+        return self._other_elements.get("timelineZoom")
+    @property
+    def Version(self) -> Optional[str]:
+        return self._other_elements.get("version")
+    @property
+    def VideoClips(self):
+        return self._videoClips
+    @property
+    def VideoStandard(self) -> Optional[str]:
+        return self._other_elements.get("videoStandard")
+    @property
+    def VideoTrashClips(self):
+        return self._videoTrashClips
+    @property
+    def WritingApplicationName(self) -> Optional[str]:
+        return self._other_elements.get("writingApplicationName")
+    @property
+    def WritingApplicationVersion(self) -> Optional[str]:
+        return self._other_elements.get("writingApplicationVersion")
+
+    def _getAudioClips(xmltree)
+    def _getAudioTrashClips(xmltree)
+    def _getVideoClips(xmltree)
+    def _getVideoTrashClips(xmltree)
---
+++ main.py
@@ -0,0 +1,20 @@
+# builtin imports
+from pathlib import Path
+from typing import Any, Dict
+from xml.etree import ElementTree as ET
+from xml.etree.ElementTree import ElementTree, Element
+# 3rd-party imports
+import bpy
+# local imports
+from utils import iMovieProj, XMLDict
+
+X_RESOLUTION = 640
+Y_RESOLUTION = 480
+BASE_FILEPATH = Path("/Volumes/Little_Black_Box/LBB Users/LBB lukeswanson/LBB Movies")
+
+path_of_proj_file = BASE_FILEPATH / Path("Outdoor_Movie/outdoor_movie_edit.iMovieProj")
+path_of_clips = BASE_FILEPATH / Path("Outdoor_Movie/")
+
+xmltree : ElementTree    = ET.parse(path_of_proj_file)
+xmldict : Dict[str, Any] = XMLDict.XMLTreeToDict(xmltree)
+project : iMovieProj     = iMovieProj(xmldict=xmldict)
