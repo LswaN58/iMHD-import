27c33c92310d5d03ffef500a3dd44f815ed50cb0
Luke Swanson <lwswanson2@wisc.edu>
2022-12-29 12:52:59 -0600

    Fix up some imports and default to config's paths.

--- main.py
+++ main.py
@@ -2,19 +2,12 @@
 from pathlib import Path
 from typing import Any, Dict
 from xml.etree import ElementTree as ET
-from xml.etree.ElementTree import ElementTree, Element
+from xml.etree.ElementTree import ElementTree
 # 3rd-party imports
-import bpy
 # local imports
 from importer import iMovieProj, XMLDict
+from config import PROJ_PATH
 
-X_RESOLUTION = 640
-Y_RESOLUTION = 480
-BASE_FILEPATH = Path("/Volumes/Little_Black_Box/LBB Users/LBB lukeswanson/LBB Movies")
-
-path_of_proj_file = BASE_FILEPATH / Path("Outdoor_Movie/outdoor_movie_edit.iMovieProj")
-path_of_clips = BASE_FILEPATH / Path("Outdoor_Movie/")
-
-xmltree : ElementTree    = ET.parse(path_of_proj_file)
+xmltree : ElementTree    = ET.parse(PROJ_PATH)
 xmldict : Dict[str, Any] = XMLDict.XMLTreeToDict(xmltree)
 project : iMovieProj     = iMovieProj(xmldict=xmldict)
